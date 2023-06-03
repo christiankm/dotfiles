fd6ee9ed52cc2a99aa9934f643de1382eebc3d09
Christian Mitteldorf <christian@mitteldorf.dk>
2023-06-03 17:57:33 +1000

    Install homebrew

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,3 +17,29 @@ case "$OSTYPE" in
   *)        echo "unknown: $OSTYPE" ;;
 esac
 
+# Install package manager
+
+# Install Homebrew, if macOS
+if [[ "$OSTYPE" =~ ^darwin ]]; then
+  if [[ $(command -v brew) == "" ]]; then
+    echo "Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  fi
+
+  # Add Homebrew to PATH
+  if [[ $(uname -m) == 'arm64' ]]; then
+    (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/$(whoami)/.zprofile # for Apple Sillicon Mac
+    eval "$(/opt/homebrew/bin/brew shellenv)" # for Apple Sillicon Mac
+  else
+    (echo; echo 'eval "$(/usr/local/bin/brew shellenv)"') >> /Users/$(whoami)/.zprofile # for Intel Mac
+    eval "$(/usr/local/bin/brew shellenv)" # for Intel Mac
+  fi
+
+  echo "Homebrew installed to $(which brew)"
+
+  # Opt-out of Homebrew analytics
+  echo "Disabling Homebrew analytics..."
+  export HOMEBREW_NO_ANALYTICS=1
+  brew analytics off
+fi
+
