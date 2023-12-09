3cfb3482059fa433b1f13379b50603137b38b6f1
Christian Mitteldorf <christian@mitteldorf.dk>
2023-12-09 14:20:05 +1100

    Improve and rename uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -4,19 +4,16 @@ success() {
   printf "\r\033[2K  [ \033[00;32mOK\033[0m ] %s\n" "$1"
 }
 
-# Uninstall software (leave settings)
-#brew uninstall
-
 # Remove files and folders
 rm -rf $HOME/.oh-my-zsh
-rm $HOME/Brewfile
-rm $HOME/.zshrc
-rm $HOME/.p10k.zsh
-rm $HOME/.gitconfig
-rm $HOME/.gitignore
-rm $HOME/.hushlogin
-rm $HOME/.ssh/config
-rm $HOME/.vimrc
+unlink $HOME/Brewfile
+unlink $HOME/.zshrc
+unlink $HOME/.p10k.zsh
+unlink $HOME/.gitconfig
+unlink $HOME/.gitignore
+unlink $HOME/.hushlogin
+unlink $HOME/.ssh/config
+unlink $HOME/.vimrc
 rm $HOME/Library/Developer/Xcode/UserData/FontAndColorThemes
 rm $HOME/Library/Developer/Xcode/UserData/xcdebugger
 
