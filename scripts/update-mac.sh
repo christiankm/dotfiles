d7c664474d68c282eb55ce57e3061a314f3e3bd5
Christian Mitteldorf <christian.mitteldorf@bilue.com.au>
2025-05-24 19:31:39 +1000

    Add github.com host to ssh config

--- scripts/update-mac.sh
+++ scripts/update-mac.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-# Update all Homebrew packages
-brew update
-brew upgrade
-brew bundle -v
-brew cleanup
-brew doctor -v
-
 # Install system updates and installables from the Mac App Store
 sudo softwareupdate -i -a --verbose
+
+# Upgrade all Homebrew packages
+brew update
+brew upgrade
+brew cleanup
+brew doctor -v
