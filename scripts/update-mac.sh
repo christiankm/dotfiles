26c20f5f3894b850f9f12ef6ce9e08b352dea576
Christian Mitteldorf <christian.km@me.com>
2025-05-26 13:12:25 +1000

    Update update-mac script to upgrade packages/apps from all sources

--- scripts/update-mac.sh
+++ scripts/update-mac.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
 # Install system updates and installables from the Mac App Store
-sudo softwareupdate -i -a --verbose
+sudo softwareupdate --all --install --verbose
 
 # Upgrade all Homebrew packages
 brew update
 brew upgrade
+brew upgrade --cask --greedy
+mas upgrade
+
+# Clean up Homebrew
 brew cleanup
 brew doctor -v
