720121210681a345e6b75f501726fc9b9bcd21ea
Christian Mitteldorf <christian.km@me.com>
2025-12-17 12:25:58 +1100

    Install fonts via Homebrew

--- local/local.sh
+++ local/local.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # Use this file to do anything that's required on the current local machine
-# which is not common, or to provide any secret environment variables. 
+# which is not common, or to provide any secret environment variables.
 #
 # If this file exists, it is sourced in .zshrc
 #
 # Do not commit this file to the git repository. This file is ignored.
 #
-
