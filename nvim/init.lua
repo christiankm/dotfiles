a55778c1375378d2b1d1c15f36bb59644a342335
Christian Mitteldorf <christian.km@icloud.com>
2026-07-19 12:26:42 +1000

    Install render-markdown nvim plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -676,6 +676,17 @@ require("lazy").setup({
 		},
 	},
 
+  -- Render Markdown files nicer
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },            -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
+
 	-- Configure color theme (scheme)
 	-- Install by running `:Lazy sync`
 	{
